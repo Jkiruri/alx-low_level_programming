eb7884944ab2e18acef7c730c069cb55b1131779
James <jamesnjunge45@gmail.com>
2023-03-17 17:52:09 +0300

    add

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,9 @@ int main(void)
 
 			for (ch = 'a'; ch <= 'z'; ch++)
 						putchar(ch);
-				for (ch = 'A'; ch <= 'Z'; ch++)
-							putchar(ch);
-					putchar('\n');
-						return (0);
+			for (ch = 'A'; ch <= 'Z'; ch++)
+				putchar(ch);
+				putchar('\n');
+			return (0);
 }
 
