973a06b4e3cec26b04ea43e88a72aa27beea3282
James Kiruri <jamesnjunge45@gmail.com>
2023-04-03 19:10:41 +0300

    add

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _memset - fill a block of memory with a specific value
- * q: this will be the starting address of memory
- * c: this is the value that is desired
- * d: this refers to the amount if bytes
+ * @q: this will be the starting address of memory
+ * @c: this is the value that is desired
+ * @d: this refers to the amount if bytes
  * Return: changed array with new value for n bytes
  */
 char *_memset(char *q, char c, unsigned int d)
