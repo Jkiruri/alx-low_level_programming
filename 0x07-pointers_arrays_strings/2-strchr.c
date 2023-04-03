5e57cab1b943288127ff1b57c7e7f4794d47af2d
James Kiruri <jamesnjunge45@gmail.com>
2023-04-03 19:22:38 +0300

    add

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @b: This is an input
+ * @d: This is an input
+ * Return: Always 0 
+ */
+char *_strchr(char *b, char d)
+{
+	int i = 0;
+
+	for (; b[i] >= '\0'; i++)
+	{
+		if (b[i] == d)
+			return (&b[i]);
+	}
+	return (0);
+}
