98dcbb0a0b1d84a7448088e05af1144919cc9848
James <jamesnjunge45@gmail.com>
2023-04-17 21:11:48 +0300

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - a dog's basic info
- * @name:This is the first member
- * @age: This is the second member
- * @owner: This is the third member
- * Description: This is the longer description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
 struct dog
 {
@@ -13,15 +15,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
- * dog_h - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog dog_h;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_h *d);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
